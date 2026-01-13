1ac1bb463e2b2054cae19174e11a6acff8fef597
Deepak Chauhan <dchauhan2159@gmail.com>
2026-01-13 20:42:51 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-center bg-indigo-700'>Hello JAbbers</div>
+    </>
+  )
+}
+
+export default App
